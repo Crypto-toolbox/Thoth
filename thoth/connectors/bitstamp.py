5d617005eb8723d6d289e8f2575678bac8b69e53
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-30 13:59:19 +0100

    update bitstamp

--- thoth/connectors/bitstamp.py
+++ thoth/connectors/bitstamp.py
@@ -2,7 +2,6 @@
 
 import time
 import logging
-import json
 
 from thoth.core.pusher import PusherConnector
 
@@ -12,104 +11,53 @@ log = logging.getLogger(__name__)
 
 class BitstampConnector(PusherConnector):
     """Websocket Connector for the Pusher-based Bitstamp API."""
+    channel = ['live_trades%s', 'order_book%s', 'diff_order_book%s', 'live_orders%s']
 
     def __init__(self, *args, **kwargs):
         """Initialize Connector."""
-        pairs = 'btceur,eurusd,xrpusd,xrpeur,xrpbtc,ltcusd,ltceur,' \
-                'ltcbtc,ethusd,etheur,ethbtc,'.split(',')
+        pairs = 'btceur, eurusd, xrpusd, xrpeur, xrpbtc, ltcusd, ltceur, ltcbtc, ethusd, ' \
+                'etheur, ethbtc, bchusd, bcheur, bchbtc'.strip(' ').split(',')
         super(BitstampConnector, self).__init__(pairs, *args, **kwargs)
 
-    @staticmethod
-    def extract_pair(actual_channel, expected_channel):
-        """
-        Split the actual_channel on expected_channel and return the resulting pair.
-
-        :param expected_channel: str
-        :param actual_channel: str
-        :return: str
-        """
-        # This works because -for example -  splitting 'order_book' on 'order_book'
-        # yields a list of 2 empty strings
-        return actual_channel.split(expected_channel)[1].strip('_') or 'btcusd'
-
     # pylint: disable=unused-argument
-    def _base_callback(self, *, pair, channels, data):
+    def _base_callback(self, data, pair):
         """Put data on respective queue."""
-        def ticker(data):
+        pair = '' if pair == 'btcusd' else '_' + pair
+
+        trades, book, diff_book, orders = [chan % pair for chan in self.channels]
+
+        def _handle_trades(data):
             """Put data on q with correct channel name."""
-            decoded_data = json.loads(data)
-            price, size = str(decoded_data['price']), str(decoded_data['amount'])
-            side = decoded_data['type']
-            uid = ("BUID:%s" % decoded_data['buy_order_id'] + "+" +
-                   "SUID:%s" % decoded_data['sell_order_id'])
-            ts = decoded_data['timestamp']
-            trade_data = pair, price, size, side, uid, None, ts
-            msg = pair, "Trades", [trade_data]
-            self.q.put(msg)
+            topic = trades + '/data'
+            self.push(topic, data, time.time())
 
-        def book(data):
-            """Put data on q with correct channel name."""
-            unpacked_data = json.loads(data)
-            ts = str(time.time())
-            formatted_data = [[(price, size, ts) for price, size in unpacked_data['bids']],
-                              [(price, size, ts) for price, size in unpacked_data['asks']], ts]
+        def _handle_book(data):
+            topic = book + '/data'
+            self.push(topic, data, time.time())
 
-            msg = pair, "Book", formatted_data
-            self.q.put(msg)
+        def _handle_diff_book(data):
+            topic = diff_book + '/data'
+            self.push(topic, data, time.time())
 
-        def diff_book(data):
-            """Put data on q with correct channel name."""
-            msg = pair, "Raw", json.loads(data)
-            self.q.put(msg)
+        def _handle_orders_deleted(data):
+            topic = orders + '/order_deleted'
+            self.push(topic, data, time.time())
 
-        def orders_deleted(data):
-            """Put data on q with correct channel name."""
-            decoded_data = json.loads(data)
-            price, _ = decoded_data['price'], decoded_data['amount']
-            uid, ts = decoded_data['id'], decoded_data['datetime']
-            side = 'ask' if int(decoded_data['order_type']) else 'bid'
-            packed_data = pair, price, 0, side, uid, ts
-            msg = pair, "Quotes", [packed_data]
-            self.q.put(msg)
+        def _handle_orders_changed(data):
+            topic = orders + '/order_changed'
+            self.push(topic, data, time.time())
 
-        def orders_changed(data):
-            """Put data on q with correct channel name."""
-            decoded_data = json.loads(data)
-            price, size = decoded_data['price'], decoded_data['amount']
-            uid, ts = decoded_data['id'], decoded_data['datetime']
-            side = 'ask' if int(decoded_data['order_type']) else 'bid'
-            packed_data = pair, price, size, side, uid, ts
-            msg = pair, "Quotes", [packed_data]
-            self.q.put(msg)
+        def _handle_orders_created(data):
+            topic = orders + '/order_created'
+            self.push(topic, data, time.time())
 
-        def orders_created(data):
-            """Put data on q with correct channel name."""
-            decoded_data = json.loads(data)
-            price, size = decoded_data['price'], decoded_data['amount']
-            uid, ts = decoded_data['id'], decoded_data['datetime']
-            side = 'ask' if int(decoded_data['order_type']) else 'bid'
-            packed_data = pair, price, size, side, uid, ts
-            msg = pair, "Quotes", [packed_data]
-            self.q.put(msg)
-
-        channel1 = self.subscribe(channels['ticker'])
-        channel1.bind('trade', ticker)
-        channel2 = self.subscribe(channels['book'])
-        channel2.bind('data', book)
-        channel3 = self.subscribe(channels['diff_book'])
-        channel3.bind('data', diff_book)
-        channel4 = self.subscribe(channels['orders'])
-        channel4.bind('order_deleted', orders_deleted)
-        channel4.bind('order_changed', orders_changed)
-        channel4.bind('order_created', orders_created)
-
-    def _pair_callback(self, data, pair=None):
-        """Cal relevant function for given pair."""
-        if pair:
-            pair = '_' + pair
-
-        channels = {'orders': 'live_orders%s' % pair,
-                    'ticker': 'live_trades%s' % pair,
-                    'book': 'order_book%s' % pair,
-                    'diff_book': 'diff_order_book%s' % pair}
-        self._base_callback(pair=pair[1:] if pair else 'btcusd', channels=channels, data=data)
+        channel1 = self.subscribe(trades)
+        channel1.bind('trade', _handle_trades)
+        channel2 = self.subscribe(book)
+        channel2.bind('data', _handle_book)
+        channel3 = self.subscribe(diff_book)
+        channel3.bind('data', _handle_diff_book)
+        channel4 = self.subscribe(orders)
+        channel4.bind('order_deleted', _handle_orders_deleted)
+        channel4.bind('order_changed', _handle_orders_changed)
+        channel4.bind('order_created', _handle_orders_created)
