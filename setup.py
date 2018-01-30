04e6daa8340ca576246e4b9598ec1789df853f67
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-30 16:09:04 +0100

    update setuppy to use distutils

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
 
 VERSION = '1.0'
 
 setup(name='thoth',
       version=VERSION,
-      description='Cryptocurrency Trading System Data Cluster Module',
+      description='Crypto Exchange Websocket Framework',
       author='Nils Diefenbach',
       author_email='23okrs20+gitlab@mykolab.com',
-      packages=['thoth', 'thoth/connectors'],
+      packages=['thoth', 'thoth/connectors', 'thoth/core'],
       test_suite='nose.collector', tests_require=['nose', 'cython'],
       classifiers=['Development Status :: 3 - Alpha',
                    'Intended Audience :: Financial and Insurance Industry',
@@ -15,5 +15,5 @@ setup(name='thoth',
                    'Operating System :: POSIX :: Linux',
                    'Programming Language :: Python :: 3 :: Only',
                    'Topic :: Office/Business :: Financial :: Investment'],
-      install_requires=['hermes-zmq', 'websocket-client', 'requests', 'autobahn', 'pysher'],
+      install_requires=['hermes-zmq', 'pyzmq', 'requests', 'pysher'],
       package_data={'': ['*.md', '*.rst']})
