549f73f6761c4b6e65589437ce53eca40d4ff214
Nils Diefenbach <nils.diefenbach@celeraone.com>
2018-03-02 09:40:19 +0100

    properly set up init file for core sub-package

--- thoth/core/__init__.py
+++ thoth/core/__init__.py
@@ -1,12 +1,3 @@
-# Import Built-Ins
-import logging
-
-# Import Third-Party
-
-# Import Homebrew
-
-# Init Logging Facilities
-log = logging.getLogger(__name__)
-
-
-
+from thoth.core.node import DataNode
+from thoth.core.websocket import WebSocketConnector
+from thoth.core.pusher import PusherConnector
